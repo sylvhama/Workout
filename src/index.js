b5bebc2dd566ad5418b23a096cc053fb87c1cd51
Sylvain Hamann <sylvhama@gmail.com>
2017-12-30 23:47:19 -0500

    Implemented persisting state

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import throttle from 'lodash/throttle';
 import App from './components/App';
 import reducer from './reducers';
+import { loadState, cleanState, saveState } from './localStorage';
 
-const store = createStore(reducer);
+const store = createStore(reducer, cleanState(loadState()));
+
+store.subscribe(throttle(() => saveState(cleanState(store.getState()))), 1000);
 
 render(
   <Provider store={store}>
