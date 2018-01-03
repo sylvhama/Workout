f39ce9d33da22eb2e8949de2541563aee8c5e6f4
Sylvain Hamann <sylvain.hamann@ubisoft.com>
2018-01-02 21:37:39 -0500

    register sw

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import throttle from 'lodash/throttle';
 import AppContainer from './containers/AppContainer';
 import reducer from './reducers';
 import { loadState, cleanState, saveState } from './scripts/localStorage';
+import register from './scripts/createServiceWorker.js';
+
 const store = createStore(reducer, cleanState(loadState() || {}));
 
 store.subscribe(throttle(() => saveState(cleanState(store.getState()))), 1000);
@@ -16,3 +18,4 @@ render(
   </Provider>,
   document.getElementById('root')
 );
+register();
